f4863f3bbd0e1254daa6a4b4635bafc9de812aaf
Nick <nrwtaylor@gmail.com>
2022-11-20 20:23:04 -0800

    Develop WiFi manager (80) and JSON and web response on 8080.

--- nmea-air-thing.ino
+++ nmea-air-thing.ino
@@ -115,7 +115,7 @@ IRAM_ATTR void checkPosition()
 }
 
 // Set web server port number to 80
-WiFiServer server(80);
+WiFiServer server(8080);
 
 // Variable to store the HTTP request
 String header;
@@ -300,6 +300,9 @@ int elapsedTimeSensorRead = 0;
 unsigned long endTime = 0;
 unsigned long endSHTTime = 0;
 unsigned long endOneWireTime = 0;
+
+unsigned long startWebRequestTime = 0;
+unsigned long runTimeWebRequestTime = 0;
 
 OneButton button(D7, true, true);
 
@@ -742,10 +745,12 @@ void drawrest (int xpos, int ypos, unsigned char b, double a, int blen)
     lcd.print(" ");
   }
 }
-
+// Development around the WiFiManager service. Seems to be a reliable place to serve.
 void webThing() {
 
   WiFiClient client = server.available();   // Listen for incoming clients
+  boolean jsonFlag = false;
+
 
   if (client) {                             // If a new client connects,
     Serial.println("New Client.");          // print a message out in the serial port
@@ -762,7 +767,13 @@ void webThing() {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
+
+            if (header.endsWith(".json")) {
+              client.println("Content-type:text/json");
+              jsonFlag = true;
+            } else {
+              client.println("Content-type:text/html");
+            }
             client.println("Connection: close");
             client.println();
 
@@ -771,6 +782,22 @@ void webThing() {
               Serial.println("GPIO 5 on");
               // output5State = "on";
               // digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /snapshot.json") >= 0) {
+              Serial.println("snapshot");
+              jsonFlag = true;
+              client.println("{}");
+
+              //    snapshotJson();
+              // Clear the header variable
+              header = "";
+              // Close the connection
+              client.stop();
+              Serial.println("Client disconnected.");
+              Serial.println("");
+              return;
+
+              // output5State = "off";
+              //  digitalWrite(output5, LOW);
             } else if (header.indexOf("GET /5/off") >= 0) {
               Serial.println("GPIO 5 off");
               // output5State = "off";
@@ -841,6 +868,12 @@ void webThing() {
   //}
 }
 
+//void snapshotJson() {
+
+//client.println("{}");
+
+//}
+
 void displayAbout() {
 
   lcd.setCursor(0, 0);         // move cursor to   (0, 0)
@@ -910,8 +943,8 @@ void webConfigPortal() {
   WiFiManager wm;
 
   //reset settings - for testing
-  //wm.resetSettings();
-
+  wm.resetSettings();
+  // !!!!
   // set configportal timeout
   wm.setConfigPortalTimeout(timeout);
 
@@ -1062,6 +1095,8 @@ void sendPMTK(char * data) {
 
   */
   char nmeaData[255];
+  char jsonData[255];
+  char identityData[255];
   char *dollar = "$";
   char *sentenceNMEA = "PMTK";
   strcpy(nmeaData, dollar);
@@ -1690,6 +1725,16 @@ bool isUuid(String uuid) {
 
 }
 
+char mqtt_server[40];
+
+WiFiManagerParameter custom_text("<p>NMEA Air Thing</p>");
+
+//WiFiManagerParameter custom_value("elapsedTimeSensorRead");
+
+WiFiManagerParameter custom_mqtt_server("server", "mqtt_server", mqtt_server, 40);
+
+
+
 
 WiFiManager wifiManager;
 
@@ -1702,7 +1747,7 @@ void setup()
 
   big.begin();
 
-  Serial.begin(38400);
+  Serial.begin(9600);
   while (!Serial) {};
   EEPROM.begin(600);
   loadUuid();
@@ -1854,6 +1899,39 @@ void setup()
   Serial.print(nameThingWiFiAP);
   Serial.println("");
 
+  //wiFiManagerParameter custom_mqtt_server("server","mqtt_server",mqtt_server, 40);
+  wifiManager.addParameter(&custom_mqtt_server);
+
+  wifiManager.addParameter(&custom_text);
+
+  //  wifiManager.addParameter(&custom_value);
+
+  WiFiManagerParameter custom_value("hey");
+  wifiManager.addParameter(&custom_value);
+
+  //  std::vector<const char *> menu = {"wifi","param","info"};
+  //  const char * menu[] = {"wifi","setup","sep","info","exit"};
+  std::vector<const char *> menu = {"wifi", "param"};
+  wifiManager.setMenu(menu);
+
+  //wifiManager.setCustomHeadElement("<style>html{filter: invert(100%); -webkit-filter: invert(100%);}</style>");
+
+  wifiManager.setCustomHeadElement("<style></style>");
+
+  wifiManager.setShowInfoErase(false);
+
+  //wifiManager.setCustomMenuHTML("Hello");
+  // setClass
+  // setCustomMenuHTML
+  // setMenu
+  wifiManager.setTitle("NMEA AIR THING");
+  //wifiManager.setParamsPage(true);
+  // Not to be combined with setMenu.
+  // htmlEntities()
+  wifiManager.setCountry("CA");
+
+  wifiManager.setDebugOutput(false);
+
   res = wifiManager.autoConnect(nameThingWiFiAP);
   if (!res) {
     lcd.setCursor(0, 1);         // move cursor to   (2,
@@ -1897,9 +1975,6 @@ void setup()
   if (displayFlag) {
     delay(1000);
   }
-
-  //}
-
 
   //Serial.println("Before server begin.");
   server.begin();
@@ -1946,20 +2021,32 @@ void setup()
       Serial.println("WIFI Not connected.");
     }
   */
+  Serial.println("Switching to 38400 baud monitoring reporting.");
+
+  Serial.begin(38400);
 
   if (displayFlag) {
     delay(1000);
     lcd.clear();
   }
 }
-
+//https://dronebotworkshop.com/wifimanager/
 
 
 
 void loop()
 {
+
+  wifiManager.setCustomHeadElement("<style>MERP</style>");
+
+  // This is updated whenever the save button is pressed.
+  //  Serial.println(custom_mqtt_server.getValue());
+
   broadcastNMEAFlag = true;
 
+  //WiFiManagerParameter custom_value("hey");
+
+  //  wifiManager.addParameter(&custom_value);
   if (millis() > endTime && sensorReadFlag == true) {
 
     if (bme.endReading()) {
@@ -1976,9 +2063,9 @@ void loop()
 
       // call sensors.requestTemperatures() to issue a global temperature
       // request to all devices on the bus
-      Serial.print("Requesting temperatures...");
+      //Serial.print("Requesting temperatures...");
       sensors.requestTemperatures(); // Send the command to get temperatures
-      Serial.println("DONE");
+      //Serial.println("DONE");
       // After we got the temperatures, we can print them here.
       // We use the function ByIndex, and as an example get the temperature from the first sensor only.
       float tempC = sensors.getTempCByIndex(0);
@@ -1999,7 +2086,7 @@ void loop()
         Serial.println("Error: Could not read temperature data");
       }
 
-    endOneWireTime = millis();
+      endOneWireTime = millis();
 
     }
 
@@ -2035,7 +2122,200 @@ void loop()
   button.tick();
 
   // Web server (http)/
-  webThing();
+  //  webThing();
+  //////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+  WiFiClient client = server.available();   // Listen for incoming clients
+  boolean jsonFlag = false;
+
+
+  if (client) {                             // If a new client connects,
+    startWebRequestTime = millis();
+
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+
+
+
+
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+
+            if (header.indexOf(".json") >= 0) {
+              client.println("Content-type:application/json");
+              jsonFlag = true;
+            } else {
+              client.println("Content-type:text/html");
+            }
+            client.println("Connection: close");
+            client.println();
+
+
+
+            float tempC = sensors.getTempCByIndex(0);
+            float tempD = sensors.getTempCByIndex(1);
+
+            char jsonData[255];
+
+            char *dollar = "{\"tmpa\":\"";
+            char *sentenceNMEA = "XDR";
+            strcpy(jsonData, dollar);
+
+            char append[6];
+            sprintf(append, "%.2f", tempC);
+            strcat(jsonData, append);
+
+            strcat(jsonData, "\",\"tmpb\":\"");
+
+            sprintf(append, "%.2f", tempD);
+            strcat(jsonData, append);
+
+            strcat(jsonData, "\"}");
+
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              // output5State = "on";
+              // digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /snapshot.json") >= 0) {
+              Serial.println("snapshot");
+              jsonFlag = true;
+
+              //              client.println("{\"test\":\"json\",\"value\":\"hello\"}");
+              client.println(jsonData);
+              //    snapshotJson();
+
+              //return;
+            } else {
+
+
+
+              //if (jsonFlag) {
+
+              //           client.println(jsonData);
+
+              //}
+
+              if (!jsonFlag) {
+
+
+
+
+                // Display the HTML web page
+                client.println("<!DOCTYPE html><html>");
+                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                client.println("<link rel=\"icon\" href=\"data:,\">");
+                // CSS to style the on/off buttons
+                // Feel free to change the background-color and font-size attributes to fit your preferences
+                client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+                client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                client.println(".button2 {background-color: #77878A;}</style></head>");
+
+                // Web Page Heading
+                client.println("<body><h1>NMEA Air Thing</h1>");
+                client.println("<p>" + uuidStr + "</p>");
+
+
+      //          client.println(jsonData);
+
+                client.println("<br />");
+                client.print(tempC );                // Display current state, and ON/OFF buttons for GPIO 5
+                     client.println("C" );
+                client.println("<br />");
+                client.print(tempD );
+                           client.println("C" );
+                                client.println("<br />");
+
+                client.println("<br />");
+
+
+  if (WiFi.status() == WL_CONNECTED) {
+    client.println("WiFi connected.");
+  } else {
+    client.println("No WiFi.");
+  }
+
+                  client.println("<br />");
+
+
+                
+                //client.println("<p>GPIO 5 - State " + output5State + "</p>");
+                // If the output5State is off, it displays the ON button
+                // if (output5State=="off") {
+                //  client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+                // } else {
+                // client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+                //    }
+
+                // Display current state, and ON/OFF buttons for GPIO 4
+                //client.println("<p>GPIO 4 - State " + output4State + "</p>");
+                // If the output4State is off, it displays the ON button
+                //      if (output4State=="off") {
+                //      client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+                //  } else {
+                //     client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+                //   }
+runTimeWebRequestTime = millis() - startWebRequestTime;
+client.print(runTimeWebRequestTime);
+client.println("ms");
+                
+                client.println("</body></html>");
+              }
+            }
+
+
+            
+            // The HTTP response ends with another blank line
+            client.println();
+
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
 
   if (consoleFlag) {
     //Serial.println("Loop.");
